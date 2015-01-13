77672ba56f112738176bccf650dfc523720a2464
bziegler <ben@localresponse.com>
2015-01-13 15:28:44 -0500

    Added BlueCavaStats

--- src/main/java/com/localresponse/program/Launcher.java
+++ src/main/java/com/localresponse/program/Launcher.java
@@ -13,56 +13,58 @@ import com.localresponse.tapad.LargeLines;
 import com.localresponse.tapad.TapadPartition;
 import com.localresponse.tapad_load.TapadLoader;
 import com.localresponse.tapad_util.TapadUniqueLineAnalyzer;
+import com.qualia.bluecava.BlueCavaStats;
 
 
 public class Launcher {
-	
-	private static final List<Class<? extends Object>> registeredProgramClasses = new ArrayList<Class<? extends Object>>();
-	
 
-	public static void registerPrograms() {
-		register(TapadPartition.class);
+    private static final List<Class<? extends Object>> registeredProgramClasses = new ArrayList<Class<? extends Object>>();
+
+
+    public static void registerPrograms() {
+        register(TapadPartition.class);
         register(CheckUnique.class);
         register(GraphCompactor.class);
         register(TapadLoader.class);
         register(LargeLines.class);
         register(ExtractCookieLines.class);
         register(TapadUniqueLineAnalyzer.class);
-	}
+        register(BlueCavaStats.class);
+    }
 
 
-	private static void register(Class<? extends Object> aClass) {
-		registeredProgramClasses.add(aClass);
-	}
+    private static void register(Class<? extends Object> aClass) {
+        registeredProgramClasses.add(aClass);
+    }
 
 
-	public static void main(String[] args) {
-		registerPrograms();
-		
-		String runClassName = (args.length > 0) ? args[0] : "";
-		Class<? extends Object> runClass = null;
-		
-		for (Class<? extends Object> oneProgramClass : registeredProgramClasses) {
-			String progName = oneProgramClass.getSimpleName();
-			System.out.println(progName);
-			
-			if (progName.equals(runClassName) || (registeredProgramClasses.size() == 1)) {
-				runClass = oneProgramClass;
-			}
-		}
-		
-		List<String> newArgs = new ArrayList<String>(Arrays.asList(args));
-		if (newArgs.size() > 0) {
-			newArgs.remove(0);
-		}
-		
-		try {
-			Method meth = runClass.getMethod("main", String[].class);
-			String[] newArgsAry = newArgs.toArray(new String[0]);
-			meth.invoke(null, (Object) newArgsAry);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        registerPrograms();
+
+        String runClassName = (args.length > 0) ? args[0] : "";
+        Class<? extends Object> runClass = null;
+
+        for (Class<? extends Object> oneProgramClass : registeredProgramClasses) {
+            String progName = oneProgramClass.getSimpleName();
+            System.out.println(progName);
+
+            if (progName.equals(runClassName) || (registeredProgramClasses.size() == 1)) {
+                runClass = oneProgramClass;
+            }
+        }
+
+        List<String> newArgs = new ArrayList<String>(Arrays.asList(args));
+        if (newArgs.size() > 0) {
+            newArgs.remove(0);
+        }
+
+        try {
+            Method meth = runClass.getMethod("main", String[].class);
+            String[] newArgsAry = newArgs.toArray(new String[0]);
+            meth.invoke(null, (Object) newArgsAry);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
---
+++ src/main/java/com/qualia/bluecava/BlueCavaStats.java
@@ -0,0 +1,114 @@
+package com.qualia.bluecava;
+
+
+import gnu.trove.set.hash.TLongHashSet;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Splitter;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import com.localresponse.add_this_mapping.ILineProcessor;
+import com.localresponse.add_this_mapping.MultiFileLineProcessor;
+
+
+public class BlueCavaStats {
+
+    private Splitter tabSplitter = Splitter.on("\t");
+    private final HashFunction hashFunction = Hashing.murmur3_128(); // Simulates MD5 hash (but only 8 bytes)
+    private final TLongHashSet hashSet = new TLongHashSet();
+    private final DateTimeFormatter dateFmt = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss");
+    private final LocalDate now = (new DateTime(2014,12,22, 0, 0)).toLocalDate();
+    private final HashMap<Integer, Integer> daysMap = new HashMap<Integer, Integer>();
+    private final ObjectMapper mapper = new ObjectMapper();
+
+
+    private void run(String tapadDir) throws IOException {
+        File[] files = (new File(tapadDir)).listFiles();
+        List<File> list = Arrays.asList(files);
+
+        MultiFileLineProcessor multiLineProcessor = new MultiFileLineProcessor();
+        // multiLineProcessor.setUseGzip(true);
+        multiLineProcessor.processFiles(list, new ILineProcessor() {
+            public void processLine(String line, long curLine) {
+                BlueCavaStats.this.processLine(line, curLine);
+            }
+
+
+            public String getStatus() {
+                String status = BlueCavaStats.this.getStatus();
+
+                return status;
+            }
+
+        });
+    }
+
+
+    protected String getStatus() {
+        String json = null;
+        try {
+            json = mapper.writeValueAsString(daysMap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return String.format("%,10d households   %s", hashSet.size(), json);
+    }
+
+
+    protected void processLine(String line, long curLine) {
+        if (curLine == 0) {
+            return;
+        }
+
+        List<String> parts = tabSplitter.splitToList(line);
+
+        // 0 HouseholdID, 1 ConsumerID, 2 MetroCode, 3 City, 4 State, 5 Latitude, 6 Longitude, 7 HouseHoldLastSeen, 8
+        // ScreenID, 9 OS, 10 OSVersion, 11 ScreenPlatform, 12 PlatformID, 13 PlatformIDType, 14 PlatformIDLastSeen
+
+        String householdId = parts.get(0);
+        long hash = hashFunction.hashString(householdId, Charsets.UTF_8).asLong();
+        hashSet.add(hash);
+
+        String lastSeen = parts.get(7);
+        if (!lastSeen.equals("NULL")) {
+            LocalDate lastSeenDT = dateFmt.parseLocalDate(lastSeen);
+
+            Integer numDays = Integer.valueOf(Days.daysBetween(lastSeenDT, now).getDays());
+            Integer cur = daysMap.get(numDays);
+            if (cur == null)
+                cur = 0;
+            cur = cur + 1;
+            daysMap.put(numDays, cur);
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        String dir = null;
+
+        if (args.length > 0) {
+            dir = args[0];
+            System.out.println("tapadDir = " + dir);
+        }
+
+        BlueCavaStats program = new BlueCavaStats();
+        program.run(dir);
+    }
+
+}
