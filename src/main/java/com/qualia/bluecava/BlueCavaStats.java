ca426c0906bb5097148793a8fea00f3618ddde6e
bziegler <ben@localresponse.com>
2015-01-15 11:22:24 -0500

    Added more stats to BlueCavaStats

--- src/main/java/com/qualia/bluecava/BlueCavaStats.java
+++ src/main/java/com/qualia/bluecava/BlueCavaStats.java
@@ -20,8 +20,10 @@ import com.google.common.base.Charsets;
 import com.google.common.base.Splitter;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
 import com.localresponse.add_this_mapping.ILineProcessor;
 import com.localresponse.add_this_mapping.MultiFileLineProcessor;
+import com.localresponse.util.CountMap;
 
 
 public class BlueCavaStats {
@@ -29,10 +31,16 @@ public class BlueCavaStats {
     private Splitter tabSplitter = Splitter.on("\t");
     private final HashFunction hashFunction = Hashing.murmur3_128(); // Simulates MD5 hash (but only 8 bytes)
     private final TLongHashSet hashSet = new TLongHashSet();
-    private final DateTimeFormatter dateFmt = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss");
-    private final LocalDate now = (new DateTime(2014,12,22, 0, 0)).toLocalDate();
+    // private final DateTimeFormatter dateFmt = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss");
+    private final DateTimeFormatter dateFmt = DateTimeFormat.forPattern("YYYYMMdd");
+    private final LocalDate now = (new DateTime(2014, 12, 22, 0, 0)).toLocalDate();
     private final HashMap<Integer, Integer> daysMap = new HashMap<Integer, Integer>();
     private final ObjectMapper mapper = new ObjectMapper();
+
+    private final CountMap screenPlatformCounts = new CountMap();
+    private final CountMap OSCounts = new CountMap();
+    private final CountMap OSVersionCounts = new CountMap();
+    private final CountMap platformIDTypeCounts = new CountMap();
 
 
     private void run(String tapadDir) throws IOException {
@@ -57,15 +65,42 @@ public class BlueCavaStats {
     }
 
 
-    protected String getStatus() {
+    private String safeWriteValueAsString(Object obj) {
         String json = null;
         try {
             json = mapper.writeValueAsString(daysMap);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return json;
+    }
+
+
+    protected String getStatus() {
+        try {
+            String daysJson = safeWriteValueAsString(daysMap);
+            Files.write(daysJson, new File("days_histogram.json"), Charsets.UTF_8);
+
+            String screenReport = screenPlatformCounts.report();
+            String osReport = OSCounts.report();
+            String osVersionReport = OSVersionCounts.report();
+            String platformIDTypeReport = platformIDTypeCounts.report();
+
+            Files.write(screenReport, new File("screen_counts.json"), Charsets.UTF_8);
+            Files.write(osReport, new File("os_counts.json"), Charsets.UTF_8);
+            Files.write(osVersionReport, new File("os_version_counts.json"), Charsets.UTF_8);
+            Files.write(platformIDTypeReport, new File("platform_id_type_counts.json"), Charsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
-        return String.format("%,10d households   %s", hashSet.size(), json);
+        return String.format("%,10d households   (also writing files each status)", hashSet.size());
+    }
+
+
+    private void report() {
+        // TODO Auto-generated method stub
+
     }
 
 
@@ -83,7 +118,7 @@ public class BlueCavaStats {
         long hash = hashFunction.hashString(householdId, Charsets.UTF_8).asLong();
         hashSet.add(hash);
 
-        String lastSeen = parts.get(7);
+        String lastSeen = parts.get(14);
         if (!lastSeen.equals("NULL")) {
             LocalDate lastSeenDT = dateFmt.parseLocalDate(lastSeen);
 
@@ -94,6 +129,16 @@ public class BlueCavaStats {
             cur = cur + 1;
             daysMap.put(numDays, cur);
         }
+
+        String screenPlatform = parts.get(11);
+        String OS = parts.get(9);
+        String OSVersion = parts.get(9) + " " + parts.get(10);
+        String platformIDType = parts.get(13);
+
+        screenPlatformCounts.updateCount(screenPlatform);
+        OSCounts.updateCount(OS);
+        OSVersionCounts.updateCount(OSVersion);
+        platformIDTypeCounts.updateCount(platformIDType);
     }
 
 
