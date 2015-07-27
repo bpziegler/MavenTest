a75605f84d4354f8d9a1475d976e352786c86a2c
bziegler <ben@localresponse.com>
2015-07-27 10:37:51 -0400

    updated logging

--- src/main/java/com/qualia/cookie/RocksCookieScan.java
+++ src/main/java/com/qualia/cookie/RocksCookieScan.java
@@ -10,12 +10,26 @@ import com.localresponse.util.CountMap;
 
 public class RocksCookieScan {
 
+    private static long dumpStats(long start, long num, byte[] key, String valStr) {
+        long lastLog;
+        lastLog = System.currentTimeMillis();
+
+        double elap = (0.0 + System.currentTimeMillis() - start) / 1000.0;
+        double putPerSec = (num + 0.0) / elap;
+        double memUsed = (0.0 + Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
+        String keyStr = new String(key);
+        System.out.println(String.format("Num %,12d   Elap %,8.1f   Num/Sec %,12.0f   MB %6.0f   Key %-40s   Value %s",
+                num, elap, putPerSec, memUsed, keyStr, valStr));
+        return lastLog;
+    }
+
+
     public static void main(String[] args) throws RocksDBException {
         System.out.println("Starting");
         long start = System.currentTimeMillis();
 
         RocksDB.loadLibrary();
-        RocksDB db = RocksDB.open("test-db");
+        RocksDB db = RocksDB.openReadOnly("test-db");
 
         RocksIterator iter = db.newIterator();
         iter.seekToFirst();
@@ -32,16 +46,7 @@ public class RocksCookieScan {
             countMap.updateCount(valStr);
 
             if ((num % 1000 == 0) && (System.currentTimeMillis() - lastLog >= 100)) {
-                lastLog = System.currentTimeMillis();
-
-                double elap = (0.0 + System.currentTimeMillis() - start) / 1000.0;
-                double putPerSec = (num + 0.0) / elap;
-                double memUsed = (0.0 + Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())
-                        / (1024 * 1024);
-                String keyStr = new String(key);
-                System.out.println(String.format(
-                        "Num %,12d   Elap %,8.1f   Num/Sec %,12.0f   MB %6.0f   Key %-40s   Value %s", num, elap,
-                        putPerSec, memUsed, keyStr, valStr));
+                lastLog = dumpStats(start, num, key, valStr);
             }
 
             iter.next();
@@ -49,6 +54,7 @@ public class RocksCookieScan {
         }
 
         db.close();
+        lastLog = dumpStats(start, num, null, null);
 
         System.out.println(countMap.report());
 
