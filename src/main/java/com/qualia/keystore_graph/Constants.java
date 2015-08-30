e2e563cbe2f8b2f2d74410c2f14b8d8d2cc28189
bpziegler <bpziegler@gmail.com>
2015-08-30 14:36:14 -0400

    Another bad UID

--- src/main/java/com/qualia/keystore_graph/Constants.java
+++ src/main/java/com/qualia/keystore_graph/Constants.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 public class Constants {
 	
-	public static final List<String> BAD_UIDS = Collections.unmodifiableList(Arrays.asList( "0", "-1", "optout", "%24UID", "${profile_ID}" ));
+	public static final List<String> BAD_UIDS = Collections.unmodifiableList(Arrays.asList( "0", "-1", "optout", "%24UID", "${profile_ID}", "profile_id" ));
 
 }
