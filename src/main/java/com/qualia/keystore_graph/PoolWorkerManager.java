812657c6c2e9e72c082d48ba74b2ad626beec637
bziegler <ben@localresponse.com>
2015-10-08 14:27:20 -0400

    bug fix for multi-threaded code

--- src/main/java/com/qualia/keystore_graph/PoolWorkerManager.java
+++ src/main/java/com/qualia/keystore_graph/PoolWorkerManager.java
@@ -1,5 +1,6 @@
 package com.qualia.keystore_graph;
 
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,93 +14,109 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.parboiled.common.Preconditions;
 
+
 public abstract class PoolWorkerManager<Context extends Closeable, InputT, OutputT> {
 
-	private ExecutorService service;
-	private AtomicBoolean done = new AtomicBoolean();
-	private BlockingQueue<InputT> inputQueue = new ArrayBlockingQueue<InputT>(1000);
-	private BlockingQueue<OutputT> outputQueue = new ArrayBlockingQueue<OutputT>(2000);
+    private ExecutorService service;
+    private AtomicBoolean done = new AtomicBoolean();
+    private BlockingQueue<InputT> inputQueue = new ArrayBlockingQueue<InputT>(1000);
+    private BlockingQueue<OutputT> outputQueue = new ArrayBlockingQueue<OutputT>(2000);
 
-	public abstract Context createContext();
 
-	public abstract OutputT transformInThread(Context context, InputT next);
+    public abstract Context createContext();
 
-	public void addInput(InputT input) throws InterruptedException {
-		Preconditions.checkArgNotNull(input, "input can not be null");
-		while (!inputQueue.offer(input)) {
-			checkConsume();
-		}
-	}
 
-	public abstract void consume(OutputT output);
+    public abstract OutputT transformInThread(Context context, InputT next);
 
-	public void start(int numThread) {
-		service = Executors.newFixedThreadPool(numThread);
 
-		// Create a bunch of worker threads
-		for (int i = 0; i < numThread; i++) {
-			Context context = createContext();
-			WorkerThread thread = new WorkerThread(this, context, done, inputQueue, outputQueue);
-			service.submit(thread);
-		}
-	}
+    public void addInput(InputT input) throws InterruptedException {
+        Preconditions.checkArgNotNull(input, "input can not be null");
+        while (!inputQueue.offer(input)) {
+            checkConsume();
+        }
+    }
 
-	public void stop() throws InterruptedException {
-		checkConsume();
-		done.set(true);
-		service.shutdown();
-		service.awaitTermination(100, TimeUnit.DAYS);
-		checkConsume();
-	}
 
-	private void checkConsume() {
-		List<OutputT> drained = new ArrayList<OutputT>();
-		outputQueue.drainTo(drained);
-		for (OutputT oneOutput : drained) {
-			consume(oneOutput);
-		}
-	}
+    public abstract void consume(OutputT output);
 
-	private static class WorkerThread<Context extends Closeable, InputT, OutputT> implements Runnable {
 
-		private final PoolWorkerManager<Context, InputT, OutputT> parent;
-		private final Context context;
-		private final AtomicBoolean done;
-		private final BlockingQueue<InputT> inputQueue;
-		private final BlockingQueue<OutputT> outputQueue;
+    public void start(int numThread) {
+        service = Executors.newFixedThreadPool(numThread);
 
-		public WorkerThread(PoolWorkerManager<Context, InputT, OutputT> parent, Context context, AtomicBoolean done, BlockingQueue<InputT> inputQueue,
-				BlockingQueue<OutputT> outputQueue) {
-			this.parent = parent;
-			this.context = context;
-			this.done = done;
-			this.inputQueue = inputQueue;
-			this.outputQueue = outputQueue;
-		}
+        // Create a bunch of worker threads
+        for (int i = 0; i < numThread; i++) {
+            Context context = createContext();
+            WorkerThread thread = new WorkerThread(this, context, done, inputQueue, outputQueue);
+            service.submit(thread);
+        }
+    }
 
-		@Override
-		public void run() {
-			while (true) {
-				try {
-					InputT next = inputQueue.poll(1, TimeUnit.MILLISECONDS);
-					if ((next == null) && (done.get() == true))
-						break;
-					if (next != null) {
-						OutputT output = parent.transformInThread(context, next);
-						outputQueue.put(output);
-					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-					throw new RuntimeException(e);
-				}
-			}
-			try {
-				context.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-				throw new RuntimeException(e);
-			}
-		}
-	}
+
+    public void stop() throws InterruptedException {
+        checkConsume();
+        done.set(true);
+        service.shutdown();
+        service.awaitTermination(100, TimeUnit.DAYS);
+        checkConsume();
+    }
+
+
+    private void checkConsume() {
+        List<OutputT> drained = new ArrayList<OutputT>();
+        outputQueue.drainTo(drained);
+        for (OutputT oneOutput : drained) {
+            consume(oneOutput);
+        }
+    }
+
+    private static class WorkerThread<Context extends Closeable, InputT, OutputT> implements Runnable {
+
+        private final PoolWorkerManager<Context, InputT, OutputT> parent;
+        private final Context context;
+        private final AtomicBoolean done;
+        private final BlockingQueue<InputT> inputQueue;
+        private final BlockingQueue<OutputT> outputQueue;
+
+
+        public WorkerThread(PoolWorkerManager<Context, InputT, OutputT> parent, Context context, AtomicBoolean done,
+                BlockingQueue<InputT> inputQueue, BlockingQueue<OutputT> outputQueue) {
+            this.parent = parent;
+            this.context = context;
+            this.done = done;
+            this.inputQueue = inputQueue;
+            this.outputQueue = outputQueue;
+        }
+
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    InputT next = inputQueue.poll(1, TimeUnit.MILLISECONDS);
+                    if ((next == null) && (done.get() == true)) {
+                        // we have to check again because more messages may have been added between the first poll and
+                        // checking the done flag
+                        next = inputQueue.poll(0, TimeUnit.MILLISECONDS);
+                        if (next == null) {
+                            break;
+                        }
+                    }
+                    if (next != null) {
+                        OutputT output = parent.transformInThread(context, next);
+                        outputQueue.put(output);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+            try {
+                context.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
 }
